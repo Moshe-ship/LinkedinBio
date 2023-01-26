24dff4d6004cd2f9d0c4ede45827cc65d660c55d
Mousa Abumazin <mussa.abumazin@gmail.com>
2023-01-26 11:42:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -30,5 +30,5 @@ def generate_bio():
     message = completions.choices[0].text
     return jsonify({"bio": message})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == '__main__': 
+    app.run()
